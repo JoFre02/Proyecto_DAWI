4af984fe3035fdbc09f1bf0eb42fc980167c4cd1
G0H4NFB <balabarcajohan@gmail.com>
2023-11-21 18:41:09 -0500

    Primera Prueba

---
+++ src/test/java/com/teatro/proyecto/ProyectoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teatro.proyecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
